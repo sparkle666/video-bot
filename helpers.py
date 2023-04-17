3c10cab6209a4214cbaf58c23249f8dcdf1e9145
Sixtus Anyanwu <sikky606@gmail.com>
2023-04-17 17:53:33 +0100

    added new func to helpers.py

--- helpers.py
+++ helpers.py
@@ -4,7 +4,6 @@ from PIL import ImageOps, Image
 import logging
 
 
-
 def get_image_data(image) -> tuple:
   """ Gets all necesary data from image """
   try:
@@ -13,24 +12,24 @@ def get_image_data(image) -> tuple:
   except Exception as e:
   		logging.exception("Failed to resize", e)
   
-  
-def get_video_data(video) -> dict:
-  """ Returns the video duration of a video in seconds
-  """
-  
-  cmd = ["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", f"{video}"]
-# Run the ffprobe command using subprocess
-  try:
-      duration = subprocess.check_output(cmd, stderr=subprocess.STDOUT, text=True)
-      dimensions_cmd = ["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0:s=x", f"{video}"]
-      dimensions_output = subprocess.check_output(dimensions_cmd).decode("utf-8").strip().split("x")
-      width = int(dimensions_output[0])
-      height = int(dimensions_output[1])
-      
-      print(width, height, duration)
-      return {"duration": float(duration), "dimension" : [width, height]}
-  except subprocess.CalledProcessError as e:
-      print("Error:", e.output)
+
+def get_video_data(video, isVideo=True) -> dict:
+    """ Returns the video duration of a video in seconds """
+    cmd = ["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", f"{video}"]
+    # Run the ffprobe command using subprocess
+    try:
+        duration = subprocess.check_output(cmd, stderr=subprocess.STDOUT, text=True)
+        if isVideo:
+            dimensions_cmd = ["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0:s=x", f"{video}"]
+            dimensions_output = subprocess.check_output(dimensions_cmd).decode("utf-8").strip().split("x")
+            print(dimensions_output)
+            width = int(dimensions_output[0])
+            height = int(dimensions_output[1])
+            return {"duration": float(duration), "dimension": [width, height]}
+        return {"duration": float(duration)}
+    except subprocess.CalledProcessError as e:
+        print("Error:", e.output)
+
 
 
 def resize_(image, resized_filename: str, isVideo = False, width = 0, height = 0, x = 0, y = 0) -> bool:
@@ -55,3 +54,35 @@ def resize_(image, resized_filename: str, isVideo = False, width = 0, height = 0
           return True
       except Exception as e:
           print("Error resizing picture", e)
+
+def duplicate_file(filename, amount = 2):
+	if os.path.exists(filename):
+		splitted_filename = filename.split(".")[0]
+		extension = filename.split(".")[1] 
+		cloned_files = []
+		try:
+				original_file = open(filename, "rb")
+				print("____Duplicating Files___")
+				for num in list(range(amount)):
+					duplicate_file = open(f"{splitted_filename}_{num}.{extension}", "wb")
+					duplicate_file.write(original_file.read())
+					duplicate_file.close()
+					cloned_files.append(f"{splitted_filename}_{num}.{extension}")
+					original_file.seek(0)
+				original_file.close()
+				
+				return {"status": True, "duplicated_files": cloned_files}
+		except Exception as e:
+				logging.exception("ERROR:", e)
+				
+				
+def add_list_to_file(list_of_videos, filename):
+	""" Adds a list of videos to an ffmpeg compatible video """
+	try:
+			txt_file = open(f"{filename}", "w")
+			for text in len(list_of_videos):
+				txt_file.writelines(f"file {text} \n")
+			txt_file.close()
+	except Exception as e:
+		logging.exception("Error:: ", e)
+	
--- videoAI.py
+++ videoAI.py
@@ -7,11 +7,18 @@ import openai
 from PIL import ImageOps, Image
 import requests_cache
 import subprocess
-
+from google_images_search import GoogleImagesSearch
+from constants import voices
+from helpers import resize_, get_video_data, duplicate_file
 
 config = dotenv_values(".env")
 openai_org = config["OPENAI_ORG"]
 openai_key = config["OPENAI"]
+google_key = config["GOOGLE_API"]
+google_project_id = config["GOOGLE_PROJECT_ID"]
+TENOR_API = config["TENOR_API"]
+CKEY = config["CKEY"]
+elevenlabs = config["ELEVENLABS"]
 
 # 1.Enter Script, character names. 2. Generate script audio, generate subtitles from script audio 3. Use main character names to make request to tenor for names. 4. For each video, extract 1 frame as jpg. 5. Resize and crop and images. 6. Check the duration of the script audio 7. Generate 3 zoom image as lomg as the duration from audio 8. Blur the zoomed video. 9 Calculate the duration/number of images to get gow much duration to keep picture or videos. 10. Place the images/videos cropped and edited into center of the video. 11. Add audio to video and sync. 12. Add subtitles and encode video. 12. End
 
@@ -21,71 +28,84 @@ session = requests_cache.CachedSession("api_callsDB")
 
 # Todo: overlay video on top, write main function that connects all.
 
-
-voices = {'Rachel': '21m00Tcm4TlvDq8ikWAM', 'Domi': 'AZnzlk1XvdvUeBnXmlld', 'Bella': 'EXAVITQu4vr4xnSDxMaL', 'Antoni': 'ErXwobaYiN019PkySvjV', 'Elli': 'MF3mGyEYCl7XYWbV9V6O', 'Josh': 'TxGEqnHWrfWFTfGW9XjX', 'Arnold': 'VR6AewLTigWG4xSOukaG', 'Adam': 'pNInz6obpgDQGcFmaJgB', 'Sam': 'yoZ06aMxZJJ28mfd3POQ'}
-
 def main():
-  """ Main program to execute rest of the script """
-  audio_voiceover = generate_audio()
-  script = load_script()
+    """ Main program to execute rest of the script """
+    
+    script = load_script()
+    title = ""
+    if script:
+        temp_title = script.get("title").split()
+        title = temp_title[0]
+        if len(title) >= 2:
+            title = temp_title[0] + "_" + temp_title[1]
+        keywords = script.get("keywords")
+        script_content = script.get("content")
+        #audio_voiceover = generate_audio(script_content, title)
+        
+        #audio_subtitle = convert_audio_to_srt("One.mp3")
+        audio_duration = get_video_data("One.mp3", isVideo=False)
+        #print(audio_duration.get("duration"))
+        keyword_urls = download_character_videos(keywords[0], 2)
+        downloaded_videos = download_video_from_url(keywords[0], keyword_urls)
+        print(keywords[0], downloaded_videos)
+        # Get one frame from a video to use as background blur
+        first_video = downloaded_videos.get("videos")[0]
+        picture = generate_pic_from_video(first_video)
+        if audio_duration > 10:
+        	# Get one 
+        	zoomed_video = add_zoom_effect(picture, audio_duration)
+        
+        
 
-def load_script() -> str:
+def google_download_images(query):
+  """ Download Image from Google """
+  gis = GoogleImagesSearch(google_key, google_project_id)
+  _search_params = {
+    'q': query,
+    'num': 3,
+    'fileType': 'jpg',
+  }
+  gis.search(search_params=_search_params, path_to_dir =f'{os.getcwd()}/images', custom_image_name = "saitama-pics")
+
+
+#google_download_images("Saitama colored")  
+    
+def load_script() -> dict:
   """ Loads a script from a text file called script.txt"""
+
+  title = keywords = script_contents = ""
   
-  script = ""
-  if os.path.exists("scripts") == False:
-    return "Please create a script.txt filw and add your script to it..."
+  if not os.path.exists("script.txt"):
+    print("Please create a script.txt file and add your script to it...")
+    return False
   try:
       with open("script.txt", "r") as f:
         script = f.read()
-        script.replace("\n", " ")
-        return script
+        if script == "":
+          print("Please add contents to your script.txt. Its empty!!")
+          return
+        # return the cursor to the beginning to enable reading file well
+        f.seek(0)
+        txt = f.readlines()
+      
+        title = txt[0].strip("\n")
+        keywords = txt[1].strip("\n")
+        if title == "" or keywords == "":
+          print("Please add a title, keywords and script: using the recommended format.")
+          return
+        title_index = title.lower().find("title:")
+        keyword_index = keywords.lower().find("keywords:")
+        script_index = script.lower().find("script:")
+        # getting the contents
+        script_contents = script[script_index + len("script:"):].strip()
+        title = title[title_index + len("title:"):].strip()
+        keywords = keywords[keyword_index + len("keywords:"):].strip().split(",")
+
+        return {"title": title, "keywords": keywords, "content": script_contents }
   except Exception as e:
       logging.exception("Error loading script.txt", e)
       
-  
-def crop_(image, bounds: tuple, cropped_filename: str, isVideo = False, width = 0, height = 0, x = 0, y = 0) -> bool:
-  """ crops a picture on specified bounding box """
-  
-  if isVideo:
-    try:
-        os.system(f"ffmpeg -i input.mp4 -filter_complex \"crop={w}:{h}:{x}:{y}\" {cropped_filename}.mp4")
-        return True
-    except Exception as e:
-        print("Error...", e)
-  else:
-      try:
-          # Open the input image
-          input_image = Image.open(image)
-    # Crop the image
-          cropped_image = ImageOps.crop(input_image, bounds)
-      # Save the cropped image
-          cropped_image.save(f"{cropped_filename}.jpg")
-          print("Cropped successfully...")
-          return True
-      except Exception as e:
-          print("Error cropping picture", e)
-
-
-
-def get_video_data(video) -> dict:
-  """ Returns the video duration of a video in seconds
-  """
-  
-  cmd = ["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", f"{video}"]
-# Run the ffprobe command using subprocess
-  try:
-      duration = subprocess.check_output(cmd, stderr=subprocess.STDOUT, text=True)
-      dimensions_cmd = ["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0:s=x", f"{video}"]
-      dimensions_output = subprocess.check_output(dimensions_cmd).decode("utf-8").strip().split("x")
-      width = int(dimensions_output[0])
-      height = int(dimensions_output[1])
-      
-      print(width, height, duration)
-      return {"duration": float(duration), "dimension" : [width, height]}
-      
-  except subprocess.CalledProcessError as e:
-      print("Error:", e.output)
+#load_script()
 
 #get_video_data("newkoko.jpg")
   
@@ -129,55 +149,56 @@ def api_call_hook(endpoint, method, payload = None, headers = None):
   else:
     return "Request method out of scope..."
 
-def convert_audio_to_srt(audio, subtitle_name):
+def convert_audio_to_srt(audio):
   """ Gets audio, sends to open ai whisper to make translate to srt
   """
   openai.api_key = openai_key
   openai.organization = openai_org
   audio_ = None 
+  audio_name = audio.split(".")[0]
   
-  if os.path.exists(f"{audio}.mp3"):
-    audio_ = open(f"{audio}.mp3", "rb")
+  if os.path.exists(audio):
+    audio_ = open(audio, "rb")
     print("Opened audio file.")
   else:
     raise FileNotFoundError 
     
   try:
-      print("Converting audio to subtitle")
       sub = openai.Audio.transcribe("whisper-1", audio_, response_format="srt")
       print("Writing file.")
-      f = open(f"{subtitle_name}.srt","w+")
+      f = open(f"{audio_name}.srt","w")
       f.write(sub)
       f.close()
-      return f"{subtitle_name}.srt"
+      audio_.close()
+      return f"{audio}.srt"
   except Exception as e:
       logging.exception("Error creating file...")
   
   
 def add_zoom_effect(picture, time = 10):
-  """ Adds a zoom effect to video """
+  """ Adds a zoom effect to picture """
   if os.path.exists(picture):
-    os.system(f'ffmpeg -loop 1 -i {picture} -y -filter_complex "[0]scale=1200:-2,setsar=1:1[out];[out]crop=1200:670[out];[out]scale=8000:-1,zoompan=z=\'zoom+0.001\':x=iw/2-(iw/zoom/2):y=ih/2-(ih/zoom/2):d=250:s=1200x670:fps=25[out]" -acodec aac -vcodec libx264 -map "[out]" -map 0:a? -pix_fmt yuv420p -r 25 -t {10} {picture.split(".")[0]}Video.mp4')
-    return f"{picture.split('.')[0]}Video.mp4"
+    os.system(f'ffmpeg -loop 1 -i {picture} -y -filter_complex "[0]scale=1200:-2,setsar=1:1[out];[out]crop=1200:670[out];[out]scale=8000:-1,zoompan=z=\'zoom+0.001\':x=iw/2-(iw/zoom/2):y=ih/2-(ih/zoom/2):d=250:s=1200x670:fps=25[out]" -acodec aac -vcodec libx264 -map "[out]" -map 0:a? -pix_fmt yuv420p -r 25 -t {10} {picture.split(".")[0]}Zoomed.mp4')
+    return f"{picture.split('.')[0]}Zoomed.mp4"
   return None
 
   
 def generate_pic_from_video(video: str) -> str:
   """ Generates pic from video"""
   
-  os.system(f"ffmpeg -i {video}.mp4 -vf 'select=eq(n\,0)' -frames:v 1 {video}.jpg")
+  os.system(f"ffmpeg -i {video} -vf 'select=eq(n\,0)' -frames:v 1 {video}.jpg")
   
   return f"{video}.jpg"
   
 #generate_pic_from_video("zoomtest")
 
-def download_character_videos(anime_char, api_key, ckey, lmt=2):
+def download_character_videos(anime_char, lmt=2):
     """ Downloads character in list from tenor"""
     urls = []
     try:
         print("inside try")
         req = session.get(
-            "https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (anime_char, api_key, ckey, lmt))
+            "https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (anime_char, TENOR_API, CKEY, lmt))
         print(req.status_code)
         if req.status_code == 200:
             res = req.json()
@@ -197,16 +218,16 @@ def download_video_from_url(char_name, url_list):
   print(f"Dowloading videos for character: {char_name}")
   vid = []
   for url in url_list:
-    #os.system(f"ffmpeg -i {url} {char_name}{url_list.index(url)}.mp4")
+    os.system(f"ffmpeg -i {url} {char_name}{url_list.index(url)}.mp4")
     vid.append(f"{char_name}{url_list.index(url)}.mp4")
     print(vid)
   return {"status": True, "videos": vid}
 
-def create_video_zoom_blur(video, blur_strength = 6):
+def add_video_blur(video, blur_strength = 6):
   os.system(f"ffmpeg -i {video}.mp4 -vf 'boxblur={blur_strength}:1' output.mp4")
   return True
   
-def generate_audio(text):
+def generate_audio(text, filename):
   """ Todo: Get text count and convert to seconds in audio, will be used to create the video length to match audio
   """
   headers = {"xi-api-key": elevenlabs}
@@ -219,10 +240,10 @@ def generate_audio(text):
       print("making requests")
       if req.status_code == 200:
         print("Saving audio file")
-        f = open(f"{text[0:6]}.mp3", "wb")
+        f = open(f"{filename}.mp3", "wb")
         f.write(req.content)
         f.close()
-        return f"{text[0:6]}.mp3"
+        return f"{filename}.mp3"
       print("Error making request...", req.reason)
   except Exception as e:
       logging.exception("Error in request", e)
@@ -252,3 +273,9 @@ def add_subtitle_to_video(video, subtitle) -> str:
 
   
 # Project: Nairaland audio for Dairy section 
+#main()
+
+#add_zoom_effect("anotherkoko.jpg", 10)
+
+res = duplicate_file("anotherkokoZoomed.mp4", 3)
+print(res.get("duplicated_files"))
